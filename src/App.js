fd76f681319e64868d652e155702dcb7442b75e9
Denchik_Bubenchik <kianuchips@gmail.com>
2022-01-29 16:22:11 +0500

    создание компоненты и применение

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import LinkButton from "./LinkButton";
 
 function App() {
+    const array=["awertus", "почта", "Выход"]
     return (
         <div className="App">
+            <div className="header">
+                <div className="header-left">
+                    <LinkButton anus ="Сделать стартовой"/>
+                </div>
+                <div className="header-right">
+                    <LinkButton anus ="awertus"/>
+                    <LinkButton anus ="почта"/>
+                    <LinkButton anus ="Выход"/>
 
+
+                    {/*<div id="header-right-nick"><span>a</span>wertus</div>*/}
+                    {/*<div id="header-right-mail">Почта</div>*/}
+                    {/*<div id="header-right-exit">Выход</div>*/}
+                </div>
+            </div>
+            <div className="content">
+                <img id="image-content" src="image/ya_logo.png" alt="123"/>
+            </div>
+            <LinkButton anus="big anus"/>
+            <LinkButton anus="another anus"/>
+            <LinkButton/>
+            <div className="footer">
+                <p className="footer-logo"><b>ЯНДЕКС</b></p>
+            </div>
         </div>);
 }
 
---
+++ src/LinkButton.js
@@ -0,0 +1,10 @@
+import './LinkButton.css';
+const LinkButton = (props) => {
+    let text = props.anus
+    if(text === undefined) text = "nichego"
+    return <div className="general"
+                onClick={() => console.log(text)}>
+    {text}
+    </div>
+}
+export default LinkButton
