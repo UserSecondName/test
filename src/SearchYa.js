744d7f3d37ad6f87a49fb7db00f246158f44bf68
Denchik_Bubenchik <kianuchips@gmail.com>
2022-02-04 20:55:43 +0500

    Поисковик

--- src/SearchYa.js
+++ src/SearchYa.js
@@ -1,14 +1,31 @@
 import './SearchYa.css'
-const SearchYa = (props)=>{
-    const amean = props.a
-    return <div className="Sgeneral">
-        <div className="in1_1">
-            <input className="in1" type="search"/>
-        </div>
-        <div className="in2_2">
-            <input id="in2" type="button" value="Найти"/>
-        </div>
-    </div>
-}
+import React from 'react'
 
+class SearchYa extends React.Component {
+
+    state = {
+        value: ''
+    }
+         a = (event) => {
+            // console.log(event.target.value)
+            this.setState({value: event.target.value})
+        }
+         b = () => {
+            const val = this.state.value
+             console.log( val)
+        }
+        render()
+        {
+            // console.log("state: ", this.state)
+            return <div className="Sgeneral">
+                <div className="in1_1">
+                    <input className="in1" type="search" onChange={this.a}/>
+                </div>
+                <div className="in2_2">
+                    <button id="in2" onClick = {this.b} >Найти</button>
+                </div>
+            </div>
+        }
+
+}
 export default SearchYa
